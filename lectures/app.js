7bd0b660c1a460f49fc8942112eeb3bc03131533
Becca MacKenzie <rmacnz@ccs.neu.edu>
2017-05-21 12:18:34 -0400

    Beginning work on server side for lecture app

--- lectures/app.js
+++ lectures/app.js
@@ -1,3 +1,15 @@
 /**
  * Created by rolob on 5/21/2017.
  */
+module.exports = function(app) {
+
+    var todos = [
+        {title: "Grocery shopping"},
+        {title: "Plan next week"},
+        {title: "Budget"}
+    ];
+
+    app.get("/api/getTodos", function(req, res){
+        res.json(todos);
+    });
+}
--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
 
+// for lecture app
+var lectureApp = require("./lectures/app.js");
+lectureApp(app);
+
 var port = process.env.PORT || 3000;
 
 app.listen(port);
