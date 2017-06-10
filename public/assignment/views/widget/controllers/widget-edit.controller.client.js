c5df8da693748d6538b5466685be31fad9ac44aa
Becca MacKenzie <rmacnz@ccs.neu.edu>
2017-06-10 11:33:51 -0400

    Fixing some bugs with adding/editing widgets

--- public/assignment/views/widget/controllers/widget-edit.controller.client.js
+++ public/assignment/views/widget/controllers/widget-edit.controller.client.js
@@ -14,8 +14,11 @@
             model.widgetId = $routeParams["wgid"];
             model.widget = widgetService.findWidgetById(model.widgetId);
 
+            model.urlPrev = "#!/user/" + model.userId + "/website/" + model.webId + "/page/" + model.pageId + "/widget";
+            model.headerTitle = "Edit " + model.widget.widgetType.toLowerCase() + " widget";
+
             model.getWidgetEditUrl = getWidgetEditUrl;
-            model.updateWidget = updateWidget;
+            model.clickOk = updateWidget;
             model.deleteWidget = deleteWidget;
         }
 
@@ -24,13 +27,55 @@
         }
 
         function updateWidget() {
-            model.widget = widgetService.updateWidget(model.widgetId, model.widget);
-            $location.url("/user/" + model.userId + "/website/" + model.webId + "/page/" + model.pageId + "/widget");
+            var invalidMsg = validateWidgetParams();
+            if (invalidMsg != "") {
+                model.errormsg = invalidMsg;
+            } else {
+                model.widget = widgetService.updateWidget(model.widgetId, model.widget);
+                $location.url("/user/" + model.userId + "/website/" + model.webId + "/page/" + model.pageId + "/widget");
+            }
         }
 
         function deleteWidget() {
             widgetService.deleteWidget(model.widgetId);
             $location.url("/user/" + model.userId + "/website/" + model.webId + "/page/" + model.pageId + "/widget");
+        }
+
+        function validateWidgetParams() {
+            switch(model.widget.widgetType) {
+                case "HEADING":
+                    if (model.widget.size > 7 || model.widget.size < 1 || model.widget.size % 1 != 0) {
+                        return "Please enter an integer between 1 and 6 for the heading size.";
+                    } else if (model.widget.text === "") {
+                        return "Please enter some text for the heading.";
+                    } else {
+                        return "";
+                    }
+                case "IMAGE":
+                    var width = model.widget.width;
+                    if (!width.endsWith("%") || parseInt(width.substring(0, width.length - 1)) === NaN) {
+                        return "Please enter a percentage between 0% and 100% for the image width (you must use the % sign).";
+                    } else if (model.widget.url === "") {
+                        return "Please enter a URL for the image (we do not currently support file upload).";
+                    } else {
+                        return "";
+                    }
+                case "YOUTUBE":
+                    var width = model.widget.width;
+                    if (!width.endsWith("%") || parseInt(width.substring(0, width.length - 1)) === NaN) {
+                        return "Please enter a percentage between 0% and 100% for the image width.";
+                    } else if (model.widget.url === "") {
+                        return "Please enter a URL for the YouTube video.";
+                    } else {
+                        return "";
+                    }
+                case "HTML":
+                    if (model.widget.text === "") {
+                        return "Please enter some HTML in the textbox.";
+                    }
+                default:
+                    return "";
+            }
         }
     }
 
--- public/assignment/views/widget/controllers/widget-new.controller.client.js
+++ public/assignment/views/widget/controllers/widget-new.controller.client.js
@@ -34,13 +34,56 @@
 
         function createWidget() {
             if (model.widget != null) {
-                model.widget.widgetType = model.widgetType.toUpperCase();
-                widgetService.createWidget(model.pageId, model.widget);
-                $location.url("/user/" + model.userId + "/website/" + model.webId + "/page/" + model.pageId + "/widget");
+                var invalidMsg = validateWidgetParams();
+                if (invalidMsg != "") {
+                    model.errormsg = invalidMsg;
+                } else {
+                    model.widget.widgetType = model.widgetType.toUpperCase();
+                    widgetService.createWidget(model.pageId, model.widget);
+                    $location.url("/user/" + model.userId + "/website/" + model.webId + "/page/" + model.pageId + "/widget");
+                }
             } else {
                 model.errormsg = "Please enter some attributes of your new widget."
             }
         }
+
+        function validateWidgetParams() {
+            switch(model.widget.widgetType) {
+                case "HEADING":
+                    if (model.widget.size > 7 || model.widget.size < 1 || model.widget.size % 1 != 0) {
+                        return "Please enter an integer between 1 and 6 for the heading size.";
+                    } else if (model.widget.text === "") {
+                        return "Please enter some text for the heading.";
+                    } else {
+                        return "";
+                    }
+                case "IMAGE":
+                    var width = model.widget.width;
+                    if (!width.endsWith("%") || parseInt(width.substring(0, width.length - 1)) === NaN) {
+                        return "Please enter a percentage between 0% and 100% for the image width (you must use the % sign).";
+                    } else if (model.widget.url === "") {
+                        return "Please enter a URL for the image (we do not currently support file upload).";
+                    } else {
+                        return "";
+                    }
+                case "YOUTUBE":
+                    var width = model.widget.width;
+                    if (!width.endsWith("%") || parseInt(width.substring(0, width.length - 1)) === NaN) {
+                        return "Please enter a percentage between 0% and 100% for the image width.";
+                    } else if (model.widget.url === "") {
+                        return "Please enter a URL for the YouTube video.";
+                    } else {
+                        return "";
+                    }
+                case "HTML":
+                    if (model.widget.text === "") {
+                        return "Please enter some HTML in the textbox.";
+                    }
+                default:
+                    return "";
+            }
+        }
+
     }
 
 })();
