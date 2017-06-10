1b04e0d2e2f711935bfc51f8632e1545759d5873
Becca MacKenzie <rmacnz@ccs.neu.edu>
2017-06-09 22:40:01 -0400

    Assignment 3: working on widgets

--- public/assignment/views/widget/controllers/widget-edit.controller.client.js
+++ public/assignment/views/widget/controllers/widget-edit.controller.client.js
@@ -3,8 +3,35 @@
         .module("WebAppMaker")
         .controller("EditWidgetController", EditWidgetController);
 
-    function EditWidgetController() {
-        var vm = this;
+    function EditWidgetController($routeParams, $location, widgetService) {
+        var model = this;
+        init();
+
+        function init() {
+            model.userId = $routeParams["uid"];
+            model.webId = $routeParams["wid"];
+            model.pageId = $routeParams["pid"];
+            model.widgetId = $routeParams["wgid"];
+            model.widget = widgetService.findWidgetById(model.widgetId);
+
+            model.getWidgetEditUrl = getWidgetEditUrl;
+            model.updateWidget = updateWidget;
+            model.deleteWidget = deleteWidget;
+        }
+
+        function getWidgetEditUrl() {
+            return "views/widget/templates/widget-edit-" + model.widget.widgetType.toLowerCase() + ".view.client.html";
+        }
+
+        function updateWidget() {
+            model.widget = widgetService.updateWidget(model.widgetId, model.widget);
+            $location.url("/user/" + model.userId + "/website/" + model.webId + "/page/" + model.pageId + "/widget");
+        }
+
+        function deleteWidget() {
+            widgetService.deleteWidget(model.widgetId);
+            $location.url("/user/" + model.userId + "/website/" + model.webId + "/page/" + model.pageId + "/widget");
+        }
     }
 
 })();
--- public/assignment/views/widget/controllers/widget-list.controller.client.js
+++ public/assignment/views/widget/controllers/widget-list.controller.client.js
@@ -3,8 +3,39 @@
         .module("WebAppMaker")
         .controller("WidgetListController", WidgetListController);
 
-    function WidgetListController() {
-        var vm = this;
+    function WidgetListController($routeParams, $sce, widgetService) {
+        var model = this;
+        init();
+
+
+        function init() {
+            model.userId = $routeParams["uid"];
+            model.webId = $routeParams["wid"];
+            model.pageId = $routeParams["pid"];
+            model.widgetList = widgetService.findWidgetsByPageId(model.pageId);
+
+            model.trustHtml = trustHtml;
+            model.getEmbeddedYouTubeUrl = getEmbeddedYouTubeUrl;
+            model.getWidgetViewUrl = getWidgetViewUrl;
+        }
+
+        function trustHtml(html) {
+            // scrub any unsafe content
+            return $sce.trustAsHtml(html);
+        }
+
+        function getEmbeddedYouTubeUrl(url) {
+            // sample URL: https://youtu.be/AM2Ivdi9c4E
+            var splitUrl = url.split("/");
+            var videoId = splitUrl[splitUrl.length - 1];
+            var embeddedUrl = "https://www.youtube.com/embed/" + videoId;
+            console.log(embeddedUrl);
+            return $sce.trustAsResourceUrl(embeddedUrl);
+        }
+
+        function getWidgetViewUrl(widgetType) {
+            return "views/widget/templates/widget-" + widgetType.toLowerCase() + ".view.client.html";
+        }
     }
 
 })();
