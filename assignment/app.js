ffe075d6b6e79bedd3c450c049c87057f21bb168
Becca MacKenzie <rmacnz@ccs.neu.edu>
2017-06-30 13:16:22 -0400

    Fixing bug where express wasn't working correctly

--- assignment/app.js
+++ assignment/app.js
@@ -1,5 +1,5 @@
-var express = require("../express");
-express.get("/hello", sayHello);
+var app = require("../express");
+app.get("/hello", sayHello);
 
 function sayHello() {
     console.log("Hello from assignment/app.js");
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-var express = require('express');
-var app = express();
+var app = require('./express');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // configure a public directory to host static content
-app.use(express.static(__dirname + '/public'));
+app.use(app.express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
 require("./assignment/app.js");
