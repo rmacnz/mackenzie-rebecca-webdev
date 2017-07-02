f4f068bd4d94d3f2bd3f8e118caf8c4a3a36fedd
Becca MacKenzie <rmacnz@ccs.neu.edu>
2017-07-02 15:38:09 -0400

    Working on project: you can now log in and register

---
+++ public/project/services/user.service.client.js
@@ -0,0 +1,73 @@
+(function(){
+    angular
+        .module('FundiesStaffPage')
+        .factory('userService', userService);
+
+    function userService($http) {
+        var users = [
+            {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder", email: "alice@wonder.com"},
+            {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley", email: "bob@marley.com"},
+            {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia", email: "cherrygarcia@gmail.com"},
+            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi", email: "jose@ccs.neu.edu"}
+        ];
+
+        var api = {
+            createUser: createUser,
+            findUserById: findUserById,
+            findUserByUsername: findUserByUsername,
+            findUserByCredentials: findUserByCredentials,
+            updateUser: updateUser,
+            deleteUser: deleteUser
+        };
+        return api;
+
+        function createUser(user) {
+            return $http.post("/api/user", user)
+                .then(function(response) {
+                    return response.data;
+                });
+        }
+
+        // Returns the user with this ID
+        function findUserById(userId) {
+            var url = "/api/user/" + userId;
+            return $http.get(url)
+                .then(function(response) {
+                    return response.data;
+                });
+        }
+
+        function findUserByUsername(username) {
+            var url = "/api/username?username=" + username;
+            return $http.get(url)
+                .then(function(response){
+                    return response.data;
+                });
+        }
+
+        // Returns the user with the given username and password
+        function findUserByCredentials(username, password) {
+            var url = "/api/user?username=" + username + "&password=" + password;
+            return $http.get(url)
+                .then(function(response) {
+                    return response.data;
+                });
+        }
+
+        function updateUser(userId, user) {
+            var url = "/api/user/" + userId;
+            return $http.put(url, user)
+                .then(function (response) {
+                    return response.data;
+                });
+        }
+
+        function deleteUser(userId) {
+            var url = "/api/user/" + userId;
+            return $http.delete(url)
+                .then(function (response) {
+                    return response.data;
+                });
+        }
+    }
+})();
--- public/project/views/user/controllers/register.controller.client.js
+++ public/project/views/user/controllers/register.controller.client.js
@@ -9,18 +9,35 @@
 
         function register() {
             if (model.user != null && model.user.username != null && model.user.password != null) {
-                var findUsername = userService.findUserByUsername(model.user.username);
-                if (findUsername != null) {
-                    model.errormsg = "A user with this username already exists. Please choose another username or go to the login page if you already have an account."
-                } else if (model.user.password === model.user.password_verify) {
-                    model.user = userService.createUser(model.user);
-                    $location.url("/user/" + model.user._id)
-                } else {
-                    model.errormsg = "Passwords did not match. Please try again."
-                }
+                userService.findUserByUsername(model.user.username)
+                    .then(nameSuccess, nameFail);
             } else {
                 model.errormsg = "Please enter a username and password."
             }
+        }
+
+        function nameSuccess(user) {
+            model.errormsg = "A user with this username already exists. Please choose another username or go to the login page if you already have an account."
+        }
+
+        function nameFail(error) {
+            if (model.user.password === model.user.password_verify) {
+                //model.user = userService.createUser(model.user);
+                userService.createUser(model.user)
+                    .then(createSuccess, createFail);
+            } else {
+                model.errormsg = "Passwords did not match. Please try again."
+            }
+        }
+
+        function createSuccess(user) {
+            model.user = user;
+            $location.url("/user/" + user._id);
+        }
+
+        function createFail(error) {
+            console.log("Failed to create user");
+            console.log(error);
         }
     }
     
