7c5557d4d54d2ff1bfb130de7603d31654de9f1f
Becca MacKenzie <rmacnz@ccs.neu.edu>
2017-07-17 11:06:58 -0400

    Working on project deliverable 3: POC. Having trouble with authentication on the google calendar API

--- public/project/app.js
+++ public/project/app.js
@@ -1,4 +1,31 @@
 (function(){
     angular
-        .module("FundiesStaffPage",["ngRoute"]);
+        .module("FundiesStaffPage",["ngRoute"])
+        .controller("FSPController", fspController);
+
+    function fspController($http) {
+        var model = this;
+        model.searchCalendar = searchCalendar;
+        model.searchDetails = searchDetails;
+
+        function searchCalendar(searchTerm) {
+            // TODO: Update to search the Fundies 1 calendar for an event with this name
+            var calendarListUrl = "https://www.googleapis.com/calendar/v3/users/me/calendarList";
+            $http.get(calendarListUrl)
+                .then(function(response) {
+                    console.log(response);
+                    model.results = response.data.Search;
+                });
+        }
+
+        function searchDetails(event) {
+            // TODO: Update to search the Fundies 1 calendar for the given event
+            var calendarListUrl = "https://www.googleapis.com/calendar/v3/users/me/calendarList";
+            $http.get(calendarListUrl)
+                .then(function(response) {
+                    console.log(response);
+                    model.detailedResult = response.data;
+                });
+        }
+    };
 })();
