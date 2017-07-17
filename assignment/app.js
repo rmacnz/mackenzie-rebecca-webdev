a828bc0514927df6ba07076d980288fac55f3c25
Becca MacKenzie <rmacnz@ccs.neu.edu>
2017-07-17 12:03:29 -0400

    Fixing non local deploy of assignment

--- assignment/app.js
+++ assignment/app.js
@@ -1,7 +1,15 @@
 var app = require("../express");
 var mongoose = require("mongoose");
 mongoose.Promise = require("q").Promise;
-mongoose.connect("mongodb://localhost/webdev");
+var connectionString = 'mongodb://localhost/webdev'; // for local
+
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds157349.mlab.com:57349/heroku_zhk37gk1';
+}
+mongoose.connect(connectionString);
 
 require("./services/user.service.server");
 require("./services/website.service.server");
