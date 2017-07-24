14a429b6769df458afa1ae42fadda2b26dcb345d
Becca MacKenzie <rmacnz@ccs.neu.edu>
2017-07-24 12:22:57 -0400

    Working on project: trying to get google calendar API to work on heroku

--- project/services/calendar.service.server.js
+++ project/services/calendar.service.server.js
@@ -13,7 +13,7 @@ var googleAuth = require('google-auth-library');
 var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 /*var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/';*/
-var TOKEN_DIR = "../.credentials/";
+var TOKEN_DIR = "credentials/";
 var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
 
 
