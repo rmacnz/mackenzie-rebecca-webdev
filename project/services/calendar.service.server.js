1525fd98526ff47329634b1b4098c0e4c3a63684
Becca MacKenzie <rmacnz@ccs.neu.edu>
2017-07-24 13:39:44 -0400

    Trying to get API to work on heroku

--- project/services/calendar.service.server.js
+++ project/services/calendar.service.server.js
@@ -106,14 +106,22 @@ function authorize(credentials, callback) {
     var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
     // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, function(err, token) {
-        if (err) {
-            getNewToken(oauth2Client, callback);
-        } else {
-            oauth2Client.credentials = JSON.parse(token);
-            return callback(oauth2Client);
-        }
-    });
+
+    var credentials = {};
+    if (process.env.GCAPI_ACCESS_TOKEN) {
+        credentials.access_token = process.env.GCAPI_ACCESS_TOKEN;
+        credentials.refresh_token = process.env.GCAPI_REFRESH_TOKEN;
+    } else {
+        fs.readFile(TOKEN_PATH, function (err, token) {
+            if (err) {
+                getNewToken(oauth2Client, callback);
+            } else {
+                credentials = JSON.parse(token);
+            }
+        });
+    }
+    oauth2Client.credentials = credentials;
+    return callback(oauth2Client);
 }
 
 /**
@@ -154,15 +162,22 @@ function getNewToken(oauth2Client, callback) {
  * @param {Object} token The token to store to disk.
  */
 function storeToken(token) {
-    try {
-        fs.mkdirSync(TOKEN_DIR);
-    } catch (err) {
-        if (err.code != 'EEXIST') {
-            throw err;
+    if (process.env.GCAPI_ACCESS_TOKEN) {
+        var newToken = JSON.stringify(token);
+        process.env.GCAPI_ACCESS_TOKEN = newToken.access_token;
+        process.env.GCAPI_REFRESH_TOKEN = newToken.refresh_token;
+        console.log("Token stored to enviroment variables");
+    } else {
+        try {
+            fs.mkdirSync(TOKEN_DIR);
+        } catch (err) {
+            if (err.code != 'EEXIST') {
+                throw err;
+            }
         }
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+        console.log('Token stored to ' + TOKEN_PATH);
     }
-    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-    console.log('Token stored to ' + TOKEN_PATH);
 }
 
 /**
