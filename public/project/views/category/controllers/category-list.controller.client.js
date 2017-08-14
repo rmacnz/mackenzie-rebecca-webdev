208fb640caf354f8cf868404b48859b1b7972e2c
Becca MacKenzie <rmacnz@ccs.neu.edu>
2017-08-13 21:17:23 -0400

    Working on project: making category add better

--- public/project/views/category/controllers/category-list.controller.client.js
+++ public/project/views/category/controllers/category-list.controller.client.js
@@ -8,9 +8,15 @@
         init();
 
         function init() {
-            model.category = currentUser;
-            model.headerTitle = "Manage Categorys";
+            model.id = null;
+            model.name = null;
+            model.user = currentUser;
+            model.headerTitle = "Manage Categories";
             model.createCategory = createCategory;
+            initializeCategories();
+        }
+
+        function initializeCategories() {
             categoryService.findAllCategories()
                 .then(function (categoryList) {
                     model.categoryList = categoryList;
@@ -22,6 +28,7 @@
                 model.displayMsg = null;
                 categoryService.createCategory(model.id, model.name)
                     .then(function (response) {
+                        initializeCategories();
                         model.displayMsg = "Successfully created category '" + model.name + "'.";
                     })
             }
