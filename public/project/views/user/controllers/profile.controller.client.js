a80dabf44f6ef5d303d1f8fee5a000ee1044dab5
Becca MacKenzie <rmacnz@ccs.neu.edu>
2017-08-15 17:37:05 -0400

    Removing debugging code from profile controller

--- public/project/views/user/controllers/profile.controller.client.js
+++ public/project/views/user/controllers/profile.controller.client.js
@@ -24,12 +24,6 @@
                     .findMemberInfo(currentUser.username)
                     .then(function (info) {
                         model.memberInfo = info;
-                        if (!model.memberInfo.skillvalues) {
-                            model.memberInfo.skillvalues = [];
-                            for (i = 0; i < 27; i++) {
-                                model.memberInfo.skillvalues.push({id: i, level: i});
-                            }
-                        }
                     });
                 model.skillIdToName = skillIdToName;
             }
